be91d677645f3133b332920deea1b5ba6c2ed361
Sreedevi Pratap <Sreedevi Pratap@LAPTOP-O49DF7TP.home>
2018-01-03 09:00:44 +0100

    commit by sree2.5

---
+++ com.git.test/src/com/git/test/gitConnectionTest.java
@@ -0,0 +1,11 @@
+package com.git.test;
+
+public class gitConnectionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Git connection test 2");
+	}
+
+}
